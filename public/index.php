81cc51e2cd209f949850951fc596807384226f41
youngiggy <youngiggy@gmail.com>
2018-05-12 19:03:40 +0900

    using php-di

--- public/index.php
+++ public/index.php
@@ -3,5 +3,14 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$helloWorld = new \ExampleApp\HelloWorld();
+$containerBuilder = new \DI\ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    \ExampleApp\HelloWorld::class => \DI\create(\ExampleApp\HelloWorld::class)
+]);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(\ExampleApp\HelloWorld::class);
 $helloWorld->announce();
