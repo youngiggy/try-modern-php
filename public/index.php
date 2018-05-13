a4548197c14e4038a7eb17d507e45c956adfdd5d
youngiggy <youngiggy@gmail.com>
2018-05-13 14:08:32 +0900

    make request and request handler ready

--- public/index.php
+++ public/index.php
@@ -3,14 +3,24 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+use Relay\Relay;
+use Zend\Diactoros\ServerRequestFactory;
+
+// build container
 $containerBuilder = new \DI\ContainerBuilder();
 $containerBuilder->useAutowiring(false);
 $containerBuilder->useAnnotations(false);
 $containerBuilder->addDefinitions([
     \ExampleApp\HelloWorld::class => \DI\create(\ExampleApp\HelloWorld::class)
 ]);
-
 $container = $containerBuilder->build();
+
+// make request and request handler ready
+$middlewareQueue = [];
+
+$requestHandler = new Relay($middlewareQueue);
+$requestHandler->handle(ServerRequestFactory::fromGlobals());
+
 
 $helloWorld = $container->get(\ExampleApp\HelloWorld::class);
 $helloWorld->announce();
